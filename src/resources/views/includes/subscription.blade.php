293ed8f85b3eacc606d13c100db30fed94bbf8d3
HUET Ben <benjamin.huet.1992@gmail.com>
2017-02-16 14:18:51 +0100

    improved subscription

--- src/resources/views/includes/subscription.blade.php
+++ src/resources/views/includes/subscription.blade.php
@@ -1,13 +1,13 @@
 @if($op->attendees->where('user_id', '=', auth()->user()->id)->count())
 	@foreach($op->attendees->where('user_id', '=', auth()->user()->id) as $attendee)
 		@if($attendee->status == "yes")
-			<span class="text-bg-green" data-toggle="tooltip" data-placement="top" title="{{ trans('calendar::seat.attending_yes') }}"><b>{{ $attendee->character->characterName }}</b></span>
+			<span class="text-bg-green text-padding" data-toggle="tooltip" data-placement="top" title="{{ trans('calendar::seat.attending_yes') }}"><b>{{ $attendee->character->characterName }}</b></span>
 		@endif
 		@if($attendee->status == "maybe")
-			<span class="text-bg-orange" data-toggle="tooltip" data-placement="top" title="{{ trans('calendar::seat.attending_maybe') }}"><b>{{ $attendee->character->characterName }}</b></span>
+			<span class="text-bg-orange text-padding" data-toggle="tooltip" data-placement="top" title="{{ trans('calendar::seat.attending_maybe') }}"><b>{{ $attendee->character->characterName }}</b></span>
 		@endif
 		@if($attendee->status == "no")
-			<span class="text-bg-red" data-toggle="tooltip" data-placement="top" title="{{ trans('calendar::seat.attending_no') }}"><b>{{ $attendee->character->characterName }}</b></span>
+			<span class="text-bg-red text-padding" data-toggle="tooltip" data-placement="top" title="{{ trans('calendar::seat.attending_no') }}"><b>{{ $attendee->character->characterName }}</b></span>
 		@endif
 		@if (!$loop->last)
 			&nbsp;&nbsp;
