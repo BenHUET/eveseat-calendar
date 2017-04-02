03aad3a6d79f8362550c8c3de4b38c02edbcf601
HUET Benjamin <benjamin.huet.1992@gmail.com>
2017-04-02 13:35:39 +0200

    added colors to the importance of operation

--- src/resources/views/operation/includes/importance.blade.php
+++ src/resources/views/operation/includes/importance.blade.php
@@ -1,29 +1,11 @@
-<?php 
-	$tmp = explode('.', $op->importance);
-	$val = $tmp[0];
-	$dec = 0;
-	if (count($tmp) > 1)
-		$dec = $tmp[1];
-
-	$output = "";
-
-	for ($i = 0; $i < $val; $i++) {
-		$output .= '<i class="fa fa-star"></i>';
-	}
-
-	$left = 5;
-	if ($dec != 0) {
-		$output .= '<i class="fa fa-star-half-o"></i>';
-		$left--;
-	}
-
-	for ($i = $val; $i < $left; $i++) {
-		$output .= '<i class="fa fa-star-o"></i>';
-	}
-
-	$html = $output;
-?>
-
+@if ($op->importance == 5)
+	<span class="text-red">
+@elseif ($op->importance >= 3)
+	<span class="text-yellow">
+@else
+	<span class="text-green">
+@endif
 <span data-toggle="tooltip" data-placement="right" title="{{ $op->importance }}/5">
-<?php echo Seat\Kassie\Calendar\Helpers\Helper::ImportanceAsEmoji($op->importance, '<i class="fa fa-star"></i>', '<i class="fa fa-star-half-o"></i>', '<i class="fa fa-star-o"></i>') ?>
+	<?php echo Seat\Kassie\Calendar\Helpers\Helper::ImportanceAsEmoji($op->importance, '<i class="fa fa-star"></i>', '<i class="fa fa-star-half-o"></i>', '<i class="fa fa-star-o"></i>') ?>
+</span>
 </span>
