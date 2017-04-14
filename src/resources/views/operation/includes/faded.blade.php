b7bbe70e832d45e415321f560dbf2fe397934aaa
HUET Benjamin <benjamin.huet.1992@gmail.com>
2017-04-15 01:47:44 +0200

    changed sorting for faded operations to desc

--- src/resources/views/operation/includes/faded.blade.php
+++ src/resources/views/operation/includes/faded.blade.php
@@ -16,7 +16,7 @@
 		<th>{{ trans('calendar::seat.actions') }}</th>
 	</tr>
 	<?php $table = "faded"; ?>
-	@foreach($ops_faded->sortBy('start_at') as $op)
+	@foreach($ops_faded->sortByDesc('start_at') as $op)
 		<tr class="{{ $op->is_cancelled == 0 ? '' : 'danger' }} text-muted tr-hoverable" >
 			<td>
 				<span>{{ $op->title }}</span>
