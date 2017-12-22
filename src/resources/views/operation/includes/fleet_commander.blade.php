a94d6355fec5cbbc7a6203f12136a1d71aa7ba15
Loïc LEUILLIOT <e.elfaus@gmail.com>
2017-12-22 11:08:49 +0100

    allow to pap only fleet which are ongoing or faded

--- src/resources/views/operation/includes/fleet_commander.blade.php
+++ src/resources/views/operation/includes/fleet_commander.blade.php
@@ -1,7 +1,7 @@
 @if($op->fc_character_id)
     {!! img('character', $op->fc_character_id, 64, ['class' => 'img-circle eve-icon small-icon'], false) !!}
     <a href="{{ route('character.view.sheet', ['character_id' => $op->fc_character_id]) }}">{{ $op->fc }}</a>
-    @if($op->fc_character_id == setting('main_character_id') && $isKnownCharacter)
+    @if($op->fc_character_id == setting('main_character_id') && $isKnownCharacter && in_array($op->status, ['ongoing', 'faded']))
     <a href="{{ route('operation.paps', [$op->id]) }}" class="btn btn-xs btn-default">Pap Fleet !</a>
     @endif
 @else
