2b5c3502f07fa4788643c9b333c63818339e1035
Loïc LEUILLIOT <e.elfaus@gmail.com>
2018-01-04 13:52:11 +0100

    improve stacked chart query

--- src/Http/Controllers/CorporationController.php
+++ src/Http/Controllers/CorporationController.php
@@ -70,37 +70,33 @@ class CorporationController extends Controller {
         if (is_null($grouped))
             $grouped = false;
 
-        if (!$grouped)
-            return response()->json(
-                Pap::where('corporationID', $corporation_id)
-                   ->where('year', intval($year))
+        $paps = Pap::where('year', intval($year))
                    ->where('month', intval($month))
-                   ->leftJoin('character_character_sheets as ccs', 'ccs.characterID', 'character_id')
+                   ->join('account_api_key_info_characters as aakic', 'character_id', 'aakic.characterID')
                    ->join('calendar_operations as co', 'co.id', 'operation_id')
                    ->join('calendar_tag_operation as cto', 'cto.operation_id', 'co.id')
-                   ->join('calendar_tags as ct', 'ct.id', 'cto.tag_id')
-                   ->select('character_id', 'ccs.name', 'analytics', DB::raw('sum(value) as qty'))
-                   ->groupBy('character_id', 'ccs.name', 'analytics')
-                   ->orderBy('qty', 'desc')
-                   ->orderBy('ccs.name', 'asc')
-                   ->get()
-            );
+                   ->join('calendar_tags as ct', 'ct.id', 'cto.tag_id');
+
+        if ($grouped)
+	        $paps = $paps->where( 'aakic2.corporationID', $corporation_id )
+	                     ->join( 'person_members as pm', 'aakic.keyID', 'pm.key_id' )
+	                     ->join( 'people as p', 'pm.person_id', 'p.id' )
+	                     ->join( 'account_api_key_info_characters as aakic2', 'main_character_id', 'aakic2.characterID' )
+	                     ->select( 'main_character_id as character_id', 'main_character_name as name', 'cto.operation_id', 'analytics', 'value' )
+	                     ->groupBy( 'main_character_id', 'main_character_name', 'cto.operation_id', 'analytics', 'value' );
+        else
+	        $paps = $paps->where('corporationID', $corporation_id)
+	                     ->select('character_id', 'characterName as name', 'cto.operation_id', 'analytics', 'value')
+	                     ->groupBy('character_id', 'characterName', 'cto.operation_id', 'analytics', 'value');
 
         return response()->json(
-            Pap::where('aakic2.corporationID', $corporation_id)
-               ->where('year', intval($year))
-               ->where('month', intval($month))
-               ->join('account_api_key_info_characters as aakic', 'character_id', 'aakic.characterID')
-               ->join('person_members as pm', 'aakic.keyID', 'pm.key_id')
-               ->join('people as p', 'pm.person_id', 'p.id')
-               ->join('account_api_key_info_characters as aakic2', 'main_character_id', 'aakic2.characterID')
-               ->join('calendar_operations as co', 'co.id', 'operation_id')
-               ->join('calendar_tag_operation as cto', 'cto.operation_id', 'co.id')
-               ->join('calendar_tags as ct', 'ct.id', 'cto.tag_id')
-               ->select('main_character_id as character_id', 'main_character_name as name', 'analytics', DB::raw('sum(value) as qty'))
-               ->groupBy('main_character_id', 'main_character_name', 'analytics')
-               ->orderBy('qty', 'desc')
-               ->orderBy('main_character_name', 'asc')
-               ->get());
+        	DB::table(DB::raw("({$paps->toSql()}) as paps"))
+		        ->mergeBindings($paps->getQuery())
+		        ->select('analytics', 'character_id', 'name', DB::raw('sum(value) as qty'))
+		        ->groupBy('character_id', 'name', 'analytics')
+                ->orderBy('qty', 'desc')
+                ->orderBy('name', 'asc')
+		        ->distinct()
+                ->get());
     }
 }
