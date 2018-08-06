531af3b48abd6fb55885c5df97851644f5d3fc0b
Loïc LEUILLIOT <e.elfaus@gmail.com>
2018-08-06 22:27:37 +0200

    fix(confirmed attendees): Ensure character exists in SeAT
    Ensure character exists in SeAT before applying name and corporation resolving.

--- src/resources/views/operation/includes/cols/confirmed/corporation.blade.php
+++ src/resources/views/operation/includes/cols/confirmed/corporation.blade.php
@@ -1,2 +1,4 @@
+@if(! is_null($row->corporation))
 {!! img('corporation', $row->character->corporation_id, 64, ['class' => 'img-circle eve-icon small-icon'], false) !!}
 <span rel="id-to-name">{{ $row->character->corporation_id }}</span>
+@endif
