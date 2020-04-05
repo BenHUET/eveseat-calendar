9849c5d3eb0b9299a9be236db50c312b3844dec1
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-04-06 00:03:17 +0200

    fix: ensure badge is properly displayed in operations

--- src/resources/views/operation/includes/cols/attendees/status.blade.php
+++ src/resources/views/operation/includes/cols/attendees/status.blade.php
@@ -1,9 +1,9 @@
 @if ($row->status == "yes")
-    <span class="label label-success">{{ trans('calendar::seat.attending_yes') }}</span>
+    <span class="badge badge-success">{{ trans('calendar::seat.attending_yes') }}</span>
 @endif
 @if ($row->status == "no")
-    <span class="label label-danger">{{ trans('calendar::seat.attending_no') }}</span>
+    <span class="badge badge-danger">{{ trans('calendar::seat.attending_no') }}</span>
 @endif
 @if ($row->status == "maybe")
-    <span class="label label-warning">{{ trans('calendar::seat.attending_maybe') }}</span>
+    <span class="badge badge-warning">{{ trans('calendar::seat.attending_maybe') }}</span>
 @endif
