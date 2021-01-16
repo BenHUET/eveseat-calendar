19c96599d901812ed1bebd853a09887fe89904d6
Andrey Dubrovskikh <akturis@gmail.com>
2021-01-16 19:36:13 +0300

    update observer

--- src/Observers/OperationObserver.php
+++ src/Observers/OperationObserver.php
@@ -14,8 +14,11 @@ class OperationObserver
 {
     public function created(Operation $operation)
     {
-        if (setting('kassie.calendar.slack_integration', true) == 1 && !is_null($operation->integration))
-            Notification::send($operation, new OperationPosted());
+        if (setting('kassie.calendar.slack_integration', true) == 1 && !is_null($operation->integration)) {
+            if (setting('kassie.calendar.event.create', true) == 1) {
+                Notification::send($operation, new OperationPosted());
+            }
+        }
     }
 
     public function updating(Operation $new_operation)
@@ -23,13 +26,21 @@ class OperationObserver
         if (setting('kassie.calendar.slack_integration', true) == 1 && !is_null($new_operation->integration)) {
             $old_operation = Operation::find($new_operation->id);
             if ($old_operation->is_cancelled != $new_operation->is_cancelled) {
-                if ($new_operation->is_cancelled == true)
-                    Notification::send($new_operation, new OperationCancelled());
-                else
-                    Notification::send($new_operation, new OperationActivated());
+                if ($new_operation->is_cancelled == true) {
+                    if (setting('kassie.calendar.event.cancel', true) == 1) {
+                        Notification::send($new_operation, new OperationCancelled());
+                    }
+                }
+                else {
+                    if (setting('kassie.calendar.event.create', true) == 1) {
+                        Notification::send($new_operation, new OperationActivated());
+                    }
+                }
             }
             else {
-                Notification::send($new_operation, new OperationUpdated());
+                if (setting('kassie.calendar.event.edit', true) == 1) {                
+                    Notification::send($new_operation, new OperationUpdated());
+                }
             }
         }
     }
