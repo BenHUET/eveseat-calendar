88687d19ee60f7168061e110ddb93545ee27c755
PhaNtomBek <97723224+PhaNtomBek@users.noreply.github.com>
2022-01-20 21:40:56 +0300

    enhance notification content & ru (#66)
    * Update permissions.php
    * Update Helper.php
    * Update seat.php

---
+++ src/resources/lang/ru/permissions.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    // Character permissions
+    'character_paps_label' => 'Paps игрока',
+    'character_paps_description' => '',
+
+    // Corporation permissions
+    'corporation_paps_label' => 'Paps корпорации',
+    'corporation_paps_description' => '',
+
+    // Calendar permissions
+    'calendar_view_label' => 'Просмотр операций',
+    'calendar_view_description' => 'Разрешить просмотр операций',
+    'calendar_create_label' => 'Создание операций',
+    'calendar_create_description' => 'Разрешить создание новых операций',
+    'calendar_setup_label' => 'Setup',
+    'calendar_setup_description' => 'Разрешить изменять настройки календаря (таги, интеграции, и т.д.)',
+    'calendar_delete_all_label' => 'Удалять любые операции',
+    'calendar_delete_all_description' => 'Разрешить удалять любые операции (в том числе созданные другими)',
+    'calendar_cancel_all_label' => 'Отменять любые операции',
+    'calendar_cancel_all_description' => 'Разрешить отменять любые операции (в том числе созданные другими)',
+    'calendar_update_all_label' => 'Изменять любые операции',
+    'calendar_update_all_description' => 'Разрешить изменять любые операции (в том числе созданные другими)',
+    'calendar_close_all_label' => 'Завершать любые операции',
+    'calendar_close_all_description' => 'Разрешить завершать любые операции (в том числе созданные другими)',
+];
