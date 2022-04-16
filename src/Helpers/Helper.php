a2eba2e7140df5cfb33fba4bb460069d7aff5a13
Alkari Verende <alkariverende@fastmail.com>
2022-04-15 20:26:50 -0500

    Integrate with Google Calendar for events

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -64,7 +64,8 @@ class Helper
         $fields[trans('calendar::seat.fleet_commander')] = $op->fc ? $op->fc : trans('calendar::seat.unknown');
         $fields[trans('calendar::seat.staging_system')] = $op->staging_sys ? $op->staging_sys : trans('calendar::seat.unknown');
         $fields[trans('calendar::seat.staging_info')] = $op->staging_info ? $op->staging_info : trans('calendar::seat.unknown');
-	$fields[trans('calendar::seat.description')] = $op->description ? $op->description : trans('calendar::seat.unknown');
+        $fields[trans('calendar::seat.description')] = $op->description ? $op->description : trans('calendar::seat.unknown');
+        $fields[trans('calendar::seat.add_to_calendar')] = '<' . self::BuildAddToGoogleCalendarUrl($op) . '|' . trans('calendar::seat.google_calendar') . '>';
 
         return function ($attachment) use ($op, $url, $fields) {
             $attachment->title($op->title, $url)
@@ -74,4 +75,28 @@ class Helper
         };
     }
 
+    public static function BuildAddToGoogleCalendarUrl($op) {
+        $base_uri = "https://calendar.google.com/calendar/render";
+        $date_format = 'Ymd\THis\Z';
+        if ($op->end_at) {
+            $dates = $op->start_at->format($date_format) .
+                     '/' .
+                     $op->end_at->format($date_format);
+        } else {
+            // No event end specified. Google requires one, so set it to the same
+            // as the event start
+            $dates = $op->start_at->format($date_format) .
+            '/' .
+            $op->start_at->format($date_format);
+        }
+
+        $query = array(
+            "action" => "TEMPLATE",
+            "text" => $op->title,
+            "dates" => $dates,
+            "details" => $op->description,
+            "location" => $op->staging_sys,
+        );
+        return $base_uri . '?' . http_build_query($query);
+    }
 }
